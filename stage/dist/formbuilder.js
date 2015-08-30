2fc1eccdb2d219efec53989726720213900e7473
Prashant Sinha <prashant@ducic.ac.in>
2015-08-30 15:09:26 +0530

    Rewrote full CSS for partial controls.

--- stage/dist/formbuilder.js
+++ stage/dist/formbuilder.js
@@ -240,7 +240,7 @@ var tour;
 tour = new Shepherd.Tour({
   defaults: {
     classes: 'shepherd-theme-arrows',
-    //scrollTo: true
+    scrollTo: false
   }
 });
 
@@ -248,9 +248,12 @@ tour.addStep('add-question', {
   title: 'Adding the Survey Questions',
   text: 'You can drag and drop the questions to add them to your survey.',
   attachTo: '.fb-add-field-types right',
-  classes: 'shepherd-theme-arrows',
-  scrollTo: false,
   buttons: [
+    {
+      text: '&times;',
+      classes: 'btn-close',
+      action: tour.cancel
+    },
     {
       text: 'Next',
       action: tour.next
@@ -262,8 +265,12 @@ tour.addStep('add-question', {
   title: 'Your survey is built in this area',
   text: 'Go ahead, re arrange your questions, or click on them to customize them.',
   attachTo: '.fb-field-wrapper left',
-  classes: 'shepherd-theme-arrows',
   buttons: [
+    {
+      text: '&times;',
+      classes: 'btn-close',
+      action: tour.cancel
+    },
     {
       text: 'Next',
       action: tour.next
@@ -275,8 +282,12 @@ tour.addStep('add-question', {
   title: 'Time for the Magic to happen!',
   text: 'Once youre done, go and eat some food.',
   attachTo: '.play-now bottom',
-  classes: 'shepherd-theme-arrows',
   buttons: [
+    {
+      text: '&times;',
+      classes: 'btn-close',
+      action: tour.cancel
+    },
     {
       text: 'Next',
       action: tour.next
@@ -284,9 +295,10 @@ tour.addStep('add-question', {
   ]
 });
 
-setTimeout(function () {
-  "use strict";
-  tour.start();
+$(function () {
+  setTimeout(function () {
+    tour.start();
+  }, 1000);
 });
 
 (function() {
--- stage/src/scripts/tour.js
+++ stage/src/scripts/tour.js
@@ -3,7 +3,7 @@ var tour;
 tour = new Shepherd.Tour({
   defaults: {
     classes: 'shepherd-theme-arrows',
-    //scrollTo: true
+    scrollTo: false
   }
 });
 
@@ -11,9 +11,12 @@ tour.addStep('add-question', {
   title: 'Adding the Survey Questions',
   text: 'You can drag and drop the questions to add them to your survey.',
   attachTo: '.fb-add-field-types right',
-  classes: 'shepherd-theme-arrows',
-  scrollTo: false,
   buttons: [
+    {
+      text: '&times;',
+      classes: 'btn-close',
+      action: tour.cancel
+    },
     {
       text: 'Next',
       action: tour.next
@@ -25,8 +28,12 @@ tour.addStep('add-question', {
   title: 'Your survey is built in this area',
   text: 'Go ahead, re arrange your questions, or click on them to customize them.',
   attachTo: '.fb-field-wrapper left',
-  classes: 'shepherd-theme-arrows',
   buttons: [
+    {
+      text: '&times;',
+      classes: 'btn-close',
+      action: tour.cancel
+    },
     {
       text: 'Next',
       action: tour.next
@@ -38,8 +45,12 @@ tour.addStep('add-question', {
   title: 'Time for the Magic to happen!',
   text: 'Once youre done, go and eat some food.',
   attachTo: '.play-now bottom',
-  classes: 'shepherd-theme-arrows',
   buttons: [
+    {
+      text: '&times;',
+      classes: 'btn-close',
+      action: tour.cancel
+    },
     {
       text: 'Next',
       action: tour.next
@@ -47,7 +58,8 @@ tour.addStep('add-question', {
   ]
 });
 
-setTimeout(function () {
-  "use strict";
-  tour.start();
+$(function () {
+  setTimeout(function () {
+    tour.start();
+  }, 1000);
 });
