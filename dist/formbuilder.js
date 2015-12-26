0f0480d31bcb36f7f9acfaaf473f422107b4ecbf
Prashant Sinha <prashant@ducic.ac.in>
2015-12-27 01:42:05 +0530

    Updated the Image Picker

--- dist/formbuilder.js
+++ dist/formbuilder.js
@@ -1,7 +1,3 @@
-var Boner = {
-    //
-};
-
 (function() {
   rivets.binders.input = {
     publishes: true,
@@ -41,6 +37,293 @@ var Boner = {
       }
     }
   });
+
+}).call(this);
+
+(function() {
+  var ImagePicker, ImagePickerOption, both_array_are_equal, sanitized_options,
+    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  $.fn.extend({
+    imagepicker: function(opts) {
+      if (opts == null) {
+        opts = {};
+      }
+      return this.each(function() {
+        var select;
+        select = $(this);
+        if (select.data("picker")) {
+          select.data("picker").destroy();
+        }
+        select.data("picker", new ImagePicker(this, sanitized_options(opts)));
+        if (opts.initialized != null) {
+          return opts.initialized.call(select.data("picker"));
+        }
+      });
+    }
+  });
+
+  sanitized_options = function(opts) {
+    var default_options;
+    default_options = {
+      hide_select: true,
+      show_label: false,
+      initialized: void 0,
+      changed: void 0,
+      clicked: void 0,
+      selected: void 0,
+      limit: void 0,
+      limit_reached: void 0
+    };
+    return $.extend(default_options, opts);
+  };
+
+  both_array_are_equal = function(a, b) {
+    return $(a).not(b).length === 0 && $(b).not(a).length === 0;
+  };
+
+  ImagePicker = (function() {
+    function ImagePicker(select_element, opts1) {
+      this.opts = opts1 != null ? opts1 : {};
+      this.sync_picker_with_select = bind(this.sync_picker_with_select, this);
+      this.select = $(select_element);
+      this.multiple = this.select.attr("multiple") === "multiple";
+      if (this.select.data("limit") != null) {
+        this.opts.limit = parseInt(this.select.data("limit"));
+      }
+      this.build_and_append_picker();
+    }
+
+    ImagePicker.prototype.destroy = function() {
+      var i, len, option, ref;
+      ref = this.picker_options;
+      for (i = 0, len = ref.length; i < len; i++) {
+        option = ref[i];
+        option.destroy();
+      }
+      this.picker.remove();
+      this.select.unbind("change");
+      this.select.removeData("picker");
+      return this.select.show();
+    };
+
+    ImagePicker.prototype.build_and_append_picker = function() {
+      if (this.opts.hide_select) {
+        this.select.hide();
+      }
+      this.select.change((function(_this) {
+        return function() {
+          return _this.sync_picker_with_select();
+        };
+      })(this));
+      if (this.picker != null) {
+        this.picker.remove();
+      }
+      this.create_picker();
+      this.select.after(this.picker);
+      return this.sync_picker_with_select();
+    };
+
+    ImagePicker.prototype.sync_picker_with_select = function() {
+      var i, len, option, ref, results;
+      ref = this.picker_options;
+      results = [];
+      for (i = 0, len = ref.length; i < len; i++) {
+        option = ref[i];
+        if (option.is_selected()) {
+          results.push(option.mark_as_selected());
+        } else {
+          results.push(option.unmark_as_selected());
+        }
+      }
+      return results;
+    };
+
+    ImagePicker.prototype.create_picker = function() {
+      this.picker = $("<ul class='thumbnails image_picker_selector'></ul>");
+      this.picker_options = [];
+      this.recursively_parse_option_groups(this.select, this.picker);
+      return this.picker;
+    };
+
+    ImagePicker.prototype.recursively_parse_option_groups = function(scoped_dom, target_container) {
+      var container, i, j, len, len1, option, option_group, ref, ref1, results;
+      ref = scoped_dom.children("optgroup");
+      for (i = 0, len = ref.length; i < len; i++) {
+        option_group = ref[i];
+        option_group = $(option_group);
+        container = $("<ul></ul>");
+        container.append($("<li class='group_title'>" + (option_group.attr("label")) + "</li>"));
+        target_container.append($("<li>").append(container));
+        this.recursively_parse_option_groups(option_group, container);
+      }
+      ref1 = (function() {
+        var k, len1, ref1, results1;
+        ref1 = scoped_dom.children("option");
+        results1 = [];
+        for (k = 0, len1 = ref1.length; k < len1; k++) {
+          option = ref1[k];
+          results1.push(new ImagePickerOption(option, this, this.opts));
+        }
+        return results1;
+      }).call(this);
+      results = [];
+      for (j = 0, len1 = ref1.length; j < len1; j++) {
+        option = ref1[j];
+        this.picker_options.push(option);
+        if (!option.has_image()) {
+          continue;
+        }
+        results.push(target_container.append(option.node));
+      }
+      return results;
+    };
+
+    ImagePicker.prototype.has_implicit_blanks = function() {
+      var option;
+      return ((function() {
+        var i, len, ref, results;
+        ref = this.picker_options;
+        results = [];
+        for (i = 0, len = ref.length; i < len; i++) {
+          option = ref[i];
+          if (option.is_blank() && !option.has_image()) {
+            results.push(option);
+          }
+        }
+        return results;
+      }).call(this)).length > 0;
+    };
+
+    ImagePicker.prototype.selected_values = function() {
+      if (this.multiple) {
+        return this.select.val() || [];
+      } else {
+        return [this.select.val()];
+      }
+    };
+
+    ImagePicker.prototype.toggle = function(imagepicker_option) {
+      var new_values, old_values, selected_value;
+      old_values = this.selected_values();
+      selected_value = imagepicker_option.value().toString();
+      if (this.multiple) {
+        if (indexOf.call(this.selected_values(), selected_value) >= 0) {
+          new_values = this.selected_values();
+          new_values.splice($.inArray(selected_value, old_values), 1);
+          this.select.val([]);
+          this.select.val(new_values);
+        } else {
+          if ((this.opts.limit != null) && this.selected_values().length >= this.opts.limit) {
+            if (this.opts.limit_reached != null) {
+              this.opts.limit_reached.call(this.select);
+            }
+          } else {
+            this.select.val(this.selected_values().concat(selected_value));
+          }
+        }
+      } else {
+        if (this.has_implicit_blanks() && imagepicker_option.is_selected()) {
+          this.select.val("");
+        } else {
+          this.select.val(selected_value);
+        }
+      }
+      if (!both_array_are_equal(old_values, this.selected_values())) {
+        this.select.change();
+        if (this.opts.changed != null) {
+          return this.opts.changed.call(this.select, old_values, this.selected_values());
+        }
+      }
+    };
+
+    return ImagePicker;
+
+  })();
+
+  ImagePickerOption = (function() {
+    ImagePickerOption.prototype.template = "<div class=\"thumbnail\">\n  <img class=\"image_picker_image\" src=\"<%= url %>\">\n</div>";
+
+    function ImagePickerOption(option_element, picker, opts1) {
+      this.picker = picker;
+      this.opts = opts1 != null ? opts1 : {};
+      this.clicked = bind(this.clicked, this);
+      this.option = $(option_element);
+      this.create_node();
+    }
+
+    ImagePickerOption.prototype.destroy = function() {
+      return this.node.find(".thumbnail").unbind();
+    };
+
+    ImagePickerOption.prototype.has_image = function() {
+      return this.option.data("img-src") != null;
+    };
+
+    ImagePickerOption.prototype.is_blank = function() {
+      return !((this.value() != null) && this.value() !== "");
+    };
+
+    ImagePickerOption.prototype.is_selected = function() {
+      var select_value;
+      select_value = this.picker.select.val();
+      if (this.picker.multiple) {
+        return $.inArray(this.value(), select_value) >= 0;
+      } else {
+        return this.value() === select_value;
+      }
+    };
+
+    ImagePickerOption.prototype.mark_as_selected = function() {
+      return this.node.find(".thumbnail").addClass("selected");
+    };
+
+    ImagePickerOption.prototype.unmark_as_selected = function() {
+      return this.node.find(".thumbnail").removeClass("selected");
+    };
+
+    ImagePickerOption.prototype.value = function() {
+      return this.option.val();
+    };
+
+    ImagePickerOption.prototype.label = function() {
+      if (this.option.data("img-label")) {
+        return this.option.data("img-label");
+      } else {
+        return this.option.text();
+      }
+    };
+
+    ImagePickerOption.prototype.clicked = function() {
+      this.picker.toggle(this);
+      if (this.opts.clicked != null) {
+        this.opts.clicked.call(this.picker.select, this);
+      }
+      if ((this.opts.selected != null) && this.is_selected()) {
+        return this.opts.selected.call(this.picker.select, this);
+      }
+    };
+
+    ImagePickerOption.prototype.create_node = function() {
+      var image, thumbnail;
+      this.node = $("<li/>");
+      image = $("<img class='image_picker_image'/>");
+      image.attr("src", this.option.data("img-src"));
+      thumbnail = $("<div class='thumbnail'>");
+      thumbnail.click({
+        option: this
+      }, function(event) {
+        return event.data.option.clicked();
+      });
+      thumbnail.append(image);
+      this.node.append(thumbnail);
+      return this.node;
+    };
+
+    return ImagePickerOption;
+
+  })();
 
 }).call(this);
 
